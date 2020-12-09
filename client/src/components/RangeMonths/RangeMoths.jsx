17bf9ee40129d07537bf3ce600500ba737d80373
belichenk0andreyka <belichenko.andreyka22@gmail.com>
2020-12-09 23:32:58 +0200

    make scroll on dates

--- client/src/components/RangeMonths/RangeMoths.jsx
+++ client/src/components/RangeMonths/RangeMoths.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import { map } from 'lodash';
+import { map, get } from 'lodash';
 
 import { getDateRangeArray } from 'helpers/actionHelper';
 
 import './rangeMonths.less'
 
 const RangeMonths = (props) => {
+    const scrollBlock = React.createRef();
     const ranges = getDateRangeArray(props.dateRange);
+    React.useEffect(() => {
+        console.log(scrollBlock.current.childNodes);
+        const arrayElement = Array.from(scrollBlock.current.childNodes);
+        console.log(arrayElement);
+        if (scrollBlock.current.childNodes.length) {
+            const elements = scrollBlock.current.childNodes;
+            scrollBlock.current.scrollLeft += elements[elements.length - 1].offsetLeft;
+        }
+    }, [Array.from(get(scrollBlock.current, ['childNodes'], [])).length])
+    const scroll = () => {
+        const elements = scrollBlock.current.childNodes;
+        console.log(elements[elements.length - 1].offsetLeft);
+        scrollBlock.current.scrollLeft += elements[elements.length - 1].offsetLeft;
+    };
     return (
-        <div className='rangeMonth'>
-            {map(ranges, date => <div key={date.dateFormat}>{date.dateFormat}</div>)}
+        <div className='rangeMonth' ref={scrollBlock}>
+            {map(ranges, date => <div key={date.dateFormat} className='rangeMonth_month' onClick={scroll}>{date.dateFormat}</div>)}
         </div>
     )
 };
